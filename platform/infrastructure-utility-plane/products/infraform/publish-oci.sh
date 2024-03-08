36fcf3e66f581f183ca1d42d5fe0ecbfe1f86ed1
erdrix <alexandre.guitton@doctolib.com>
2024-03-08 18:15:41 +0100

    test workflow structure

---
+++ platform/infrastructure-utility-plane/products/infraform/publish-oci.sh
@@ -0,0 +1,37 @@
+pushd .
+
+REPOSITORY=konpyutaika/data-mesh
+
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+cd $tmp_dir
+
+git clone https://github.com/${REPO}
+cd aws-primitive-modules
+
+TAG=$(git rev-parse --short HEAD)
+SOURCE="$(git config --get remote.origin.url)"
+REVISION="$(git branch --show-current)/$(git rev-parse HEAD)"
+
+rm -rf .git || true
+rm -rf .github || true
+rm Makefile
+rm *.sh
+rm $(find . -name "README.md")
+
+flux push artifact \
+    oci://ghcr.io/${REPO}:$PROVIDER_VERSION-$TAG \
+	--path="./" \
+	--source=$SOURCE \
+	--revision=$REVISION
+
+flux tag artifact \
+    oci://ghcr.io/${REPO}:$PROVIDER_VERSION-$TAG \
+    --tag latest
+
+flux tag artifact \
+    oci://ghcr.io/${REPO}:$PROVIDER_VERSION-$TAG \
+    --tag $PROVIDER_VERSION-$1
+
+rm -rf $tmp_dir
+
+popd
