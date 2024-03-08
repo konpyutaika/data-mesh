7f3d726cb74734f9a460f2fa79e19d9e23774b96
erdrix <alexandre.guitton@doctolib.com>
2024-03-08 18:44:44 +0100

    test before publication

--- platform/infrastructure-utility-plane/products/infraform/publish-oci.sh
+++ platform/infrastructure-utility-plane/products/infraform/publish-oci.sh
@@ -1,36 +1,23 @@
 pushd .
 
+
 REPOSITORY=konpyutaika/data-mesh
-
-tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
-cd $tmp_dir
-
-git clone https://github.com/${REPO}
-cd aws-primitive-modules
-
-TAG=$(git rev-parse --short HEAD)
 SOURCE="$(git config --get remote.origin.url)"
 REVISION="$(git branch --show-current)/$(git rev-parse HEAD)"
 
-rm -rf .git || true
-rm -rf .github || true
-rm Makefile
-rm *.sh
-rm $(find . -name "README.md")
-
 flux push artifact \
-    oci://ghcr.io/${REPO}:$PROVIDER_VERSION-$TAG \
-	--path="./" \
+    oci://ghcr.io/${REPOSITORY}/$3:$TAG \
+	--path="$2" \
 	--source=$SOURCE \
 	--revision=$REVISION
 
 flux tag artifact \
-    oci://ghcr.io/${REPO}:$PROVIDER_VERSION-$TAG \
+    oci://ghcr.io/${REPOSITORY}/$3:$TAG \
     --tag latest
 
 flux tag artifact \
-    oci://ghcr.io/${REPO}:$PROVIDER_VERSION-$TAG \
-    --tag $PROVIDER_VERSION-$1
+    oci://ghcr.io/${REPOSITORY}/$3:$TAG \
+    --tag $1
 
 rm -rf $tmp_dir
 
